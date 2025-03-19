1a9f012515d66e99543ff0f14ac9920b9b94e9e6
Ghostpappi <tonyeakulasah@gmail.com>
2025-03-19 15:21:51 +0100

    You're Getting SomeWhere

--- main.go
+++ main.go
@@ -1,39 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"time"
 )
 
+// Creating a struct for my Task
+
 type Task struct {
-	Id          int
-	Status      string
-	Description string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	Id          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-func main() {
+var tasks []Task
 
-	tasks := []Task{
-		{1,
-			"My first Project",
-			"Completing my first Project",
-			time.Now().AddDate(2025, 03, 18),
-			time.Now(),
-		},
+func main() {
+	const fileName = "Task.json"
+	if err := loadTasks(fileName); err != nil {
+		fmt.Println("Error Loading Task: ", err)
+		return
 	}
-	fmt.Println(tasks)
+	fmt.Println("Welcome to Task Tracker")
+
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: task-tracker-cli [command] [args]")
+		return
+	}
+	description := os.Args[2]
 
 	newTask := Task{
-		Id:          2,
-		Status:      "My second Project",
-		Description: "Completing my Second Project",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
+		len(tasks) + 1,
+		description,
+		"not_done",
+		time.Now(),
+		time.Now(),
 	}
-
 	tasks = append(tasks, newTask)
-	fmt.Println(tasks)
+}
+
+func loadTasks(fileName string) error {
+	file, err := os.ReadFile(fileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	return json.Unmarshal(file, &tasks)
+}
+
+func saveToTask(fileName string) error {
+	data, err := json.MarshalIndent(tasks, "", "")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(fileName, data, 0644)
+}
+
+func updateTask() {
 
 }
